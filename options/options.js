45d1667bc3cf32b23ecf3e77d892ce5f586c39a8
Crowdbotics <engineering@crowdbotics.com>
2023-07-11 23:18:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mute-forest-dev-100249",
+  url: "https://mute-forest-dev-100249.botics.co",
+  api: "https://mute-forest-dev-100249.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
